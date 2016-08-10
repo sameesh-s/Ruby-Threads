eca690f00cc96b4adbc7c8ae9420dce032792e6a
sameesh-s <sameesh.fisat@gmail.com>
2016-08-10 17:10:26 +0530

    http request

--- Threads.rb
+++ Threads.rb
@@ -1,9 +1,26 @@
 #!/usr/bin/env ruby
+require 'net/http'
+require 'openssl'
 
-threads = []
-threads << Thread.new { puts "first thread" }
-threads << Thread.new { puts "second thread" }
-threads << Thread.new {3.times {puts "3 times looping thread"} }
-threads << Thread.new { puts "forth thread" }
+def get_code(address,thread)
+  uri = URI(address.to_s)
+  http = Net::HTTP.new(uri.host, uri.port)
+  if uri.scheme =='https'
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  end
+      code = http.get(uri.request_uri).code
+      puts address +" : "+ code +" : "+thread.to_s
+end
 
-threads.each { |thr| thr.join }
+@items1 = "http://3gworldagents.com"
+@items2 = "http://www.google.com/"
+@items3 = "https://3gworldagents.com/"
+
+threads = (1..3).map do |i|
+  Thread.new(i) do |i|
+    items = instance_variable_get("@items#{i}")
+    get_code(items,i)
+  end
+end
+threads.each {|t| t.join}

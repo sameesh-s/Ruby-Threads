8d949de602d137926826a2ffdb9a9b7f41c8e079
sameesh-s <sameesh.fisat@gmail.com>
2016-08-10 12:23:29 +0530

    first commit

--- Threads.rb
+++ Threads.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
 
+threads = []
+threads << Thread.new { puts "first thread" }
+threads << Thread.new { puts "second thread" }
+threads << Thread.new {3.times {puts "3 times looping thread"} }
+threads << Thread.new { puts "forth thread" }
+
+threads.each { |thr| thr.join }

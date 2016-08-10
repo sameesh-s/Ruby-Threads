d1c714a86a62b03b73c5af476afc69b0981a11c3
sameesh-s <sameesh.fisat@gmail.com>
2016-08-10 23:33:07 +0530

    mutex added

--- Threads.rb
+++ Threads.rb
@@ -3,21 +3,35 @@ require 'net/http'
 require 'openssl'
 require 'csv'
 
-def get_code(address,thread)
-  uri = URI("http://www."+address.to_s)
-  http = Net::HTTP.new(uri.host, uri.port)
-  httpcode = http.get(uri.request_uri).code
-  uri = URI("https://www."+address.to_s)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  httpscode = http.get(uri.request_uri).code
-  puts address +" : "+ httpcode +" : "+httpscode
+def write(arr)
+  CSV.open("myfile.csv","a") do |csv|
+    csv <<arr
+  end
 end
 
-@items1 = "facebook.com"
-@items2 = "google.com"
-@items3 = "amazon.in"
+def get_code(address)
+  uri =URI(address.to_s)
+  http = Net::HTTP.new(uri.host, uri.port)
+  if uri.scheme == "https"
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  end 
+  return http.get(uri.request_uri).code   
+end
+
+def get_row(address,index)
+  arr = []
+  arr.push(index.to_i)
+  arr.push(address.to_s)
+  arr.push(get_code("http://www."+address.to_s))
+  arr.push(get_code("https://www."+address.to_s))
+  arr
+end
+
+
+@address1 = "flipkart.com"
+@address2 = "google.com"
+@address3 = "amazon.in"
 =begin
 names = []
 CSV.foreach("/home/xcavenger/sandbox/Ruby-Threads/top-1m.csv") do |row|
@@ -25,11 +39,14 @@ CSV.foreach("/home/xcavenger/sandbox/Ruby-Threads/top-1m.csv") do |row|
   puts row[1]
 end
 =end
-
+mutex = Mutex.new
 threads = (1..3).map do |i|
   Thread.new(i) do |i|
-    items = instance_variable_get("@items#{i}")
-    get_code(items,i)
+    address = instance_variable_get("@address#{i}")
+    arr = get_row(address,i)
+    mutex.synchronize do
+      write(arr)
+      end
   end
 end
 threads.each {|t| t.join}
